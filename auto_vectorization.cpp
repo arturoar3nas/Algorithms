366d840a766626a098a5c3a289be39446d6d92fd
Arturo <arturoar3nas@gmail.com>
2020-03-09 18:39:36 -0300

    Update auto_vectorization.cpp

--- auto_vectorization.cpp
+++ auto_vectorization.cpp
@@ -1,14 +1,17 @@
 
 #pragma GCC optimize("O3","unroll-loops","omit-frame-pointer","inline") //Optimization flags
-#pragma GCC option("arch=native","tune=native","no-zero-upper") //Enable AVX
+#pragma GCC option("arch=skylake-avx512","tune=skylake-avx512","no-zero-upper") //Enable AVX
 #pragma GCC target("avx2")  //Enable AVX
 #include <x86intrin.h> //AVX/SSE Extensions
 #include <bits/stdc++.h> //All main STD libraries
+// #include <tbb/tbb.h>
+#include <tbb/parallel_for.h>
 
 using namespace std;
 using namespace std::chrono;
 
-// -fopt-info-vec-optimized -fstack-usage
+// -fopt-info-vec-optimized -fopt-info-vec-missed -fstack-usage -fsave-optimization-record 
+// 'foo', 'bar', 'baz', 'qux', 'quux', 'corge', 'grault', 'garply', 'waldo', 'fred', 'plugh', 'xyzzy', 'thud'
 
 // int ga[256], gb[256], gc[256];
 // void foo () {
@@ -68,25 +71,140 @@ using namespace std::chrono;
 //    }
 // }
 
+// void quux ()
+// {
+//     const float n_elements {1000000};
+//     std::vector<float> v(n_elements);
+//     for (std::vector<float>::iterator it = v.begin(); it != v.end(); it++)
+//     {
+//         (*it) += 23;
+//     }   
+// }
+
+// void corge () noexcept
+// {
+//     const float n_elements {1000000};
+//     const float n_iterations {1000};
+
+//     std::vector<float> v(n_elements);
+//     std::vector<float> elem_1(n_elements);
+//     std::vector<float> elem_2(n_elements);
+
+//     for(size_t i = 0; i < n_elements; ++i)
+//     {
+//         v[i] = elem_1[i] + elem_2[i]; 
+//     }
+// }
+
+void garply () noexcept
+{
+    auto __attribute__((aligned(32))) vec_a = std::vector<double>(10000) ;
+    auto __attribute__((aligned(32))) vec_b = std::vector<double>(10000);
+    auto __attribute__((aligned(32))) vec_c = std::vector<double>(10000);
+    auto __attribute__((aligned(32))) vec_d = std::vector<double>(10000);
+    auto __attribute__((aligned(32))) vec_f = std::vector<double>(10000);
+    auto __attribute__((aligned(32))) vec_g = std::vector<double>(10000);
+    auto __attribute__((aligned(32))) vec_h = std::vector<double>(10000);
+
+    tbb::parallel_for( tbb::blocked_range<int>(0,vec_a.size()),
+                    [&](tbb::blocked_range<int> r)
+    {
+        
+        auto __attribute__((aligned(32))) input_it = vec_a.begin();
+        auto __attribute__((aligned(32))) background_it = vec_b.begin();
+        auto __attribute__((aligned(32))) vec_c_it = vec_c.begin();
+        auto __attribute__((aligned(32))) it = vec_d.begin();
+        auto __attribute__((aligned(32))) diff_it = vec_f.begin();
+        auto __attribute__((aligned(32))) mask_it = vec_g.begin();
+        auto __attribute__((aligned(32))) output_it = vec_h.begin();
+
+        for (int i=r.begin(); i<r.end(); ++i, input_it++,
+                                                background_it++,
+                                                vec_c_it++,
+                                                it++,
+                                                diff_it++,
+                                                mask_it++,
+                                                output_it++)
+        {
+            
+
+            (*diff_it) = (*input_it)-(*background_it);
+            if (((*diff_it) > 0) && ((*input_it) > double{1.0d})){
+                (*output_it) = (((*diff_it) / (*vec_c_it)) > double{1.0d})? ( ((*mask_it)>0)? static_cast<double>(0xFF) : double{0.0d}) : double{0.0d};
+                // (*it) = ((*it) < 0.0d) ? 0.0d : (*it);
+            }
+            // else {
+                // (*it) = 0;
+                // (*output_it) = 0;
+            // }
+        }
+    });
+}
+
+// void grault() noexcept
+// {
+//     const float n_elements {1000000};
+//     const float n_iterations {1000};
+
+//     std::vector<float> v(n_elements);
+//     std::vector<float> elem_1(n_elements);
+//     std::vector<float> elem_2(n_elements);
+
+//     //for(auto i = 0; i < n_iterations; ++i) {
+//         auto it_el_1 = elem_1.begin();
+//         auto it_el_2 = elem_2.begin();
+//         for(auto it = v.begin(); it != v.end(); ++it)
+//         {
+//             (*it) = (*it_el_1) + (*it_el_2);
+//             (*it) = (*it_el_1) - (*it_el_2); 
+//             (*it) = (*it_el_1) * (*it_el_2); 
+//             (*it) = (*it_el_1) / (*it_el_2);  
+//         }
+//     //}
+// }
 
 
 int main()
 {
 
-    const float n_elements {1000000};
-    const float n_iterations {1000};
+    // const float n_elements {1000000};
+    // const float n_iterations {1000};
+
+
     
-    {
-        std::vector<float> v(n_elements);
+    // {
+    //     std::vector<float> v(n_elements);
     
-        for(size_t i = 0; i < n_iterations; ++i)
-        {
-            for(size_t j = 0; j < n_elements; ++j)
-            {
-                v[j] = j;
-            }
-        }
-    }
+    //     for(size_t i = 0; i < n_iterations; ++i)
+    //     {
+    //         for(size_t j = 0; j < n_elements; ++j)
+    //         {
+    //             v[j] = j;
+    //         }
+    //     }
+    // }
+    // {
+    //     std::vector<float> v(n_elements);
+    //     std::vector<float> elem_1(n_elements);
+    //     std::vector<float> elem_2(n_elements);
+    
+    //     for(size_t i = 0; i < n_iterations; ++i)
+    //     {
+    //         for(size_t j = 0; j < n_elements; ++j)
+    //         {
+    //             v[j] = elem_1[j] / elem_2[j];
+    //         }
+    //     }
+    // }
+
+    // {
+    //     std::vector<float> v(n_elements);
+    //     for (std::vector<float>::iterator it = v.begin(); it != v.end(); it++)
+    //     {
+    //         (*it) += 23;
+    //     }
+    // }
+
 	// const int N = 200000; //Array Size
 	// const int noTests = 10000; //Number of tests
 	// float a[N],b[N],c[N],result[N];
